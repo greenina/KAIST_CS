e59ace06172b92b758961e8df1242fbd41a662a0
greenina <igreen0485@gmail.com>
2021-01-16 13:13:00 +0900

    feature:scroll to each element

---
+++ src/components/Intro/index.js
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react'
+import './style.css'
+
+const Intro = (props)=>{
+    return(
+        <div id="intro" style={{height:500}}>
+            <h1>전산학부 학생회는 말이죠~~~~</h1>
+            짱이에요!!
+        </div>
+    )
+}
+
+export default Intro;
---
+++ src/components/Memebrs/index.js
@@ -0,0 +1,14 @@
+import React , {useRef} from 'react'
+import './style.css'
+
+const Members = (props)=>{
+    const MembersPos = useRef();
+    return(
+        <div id="members"style={{height:1000}}>
+            <h1>MEMBERS</h1>
+            전산학부 비선실세 최승연
+        </div>
+    )
+}
+
+export default Members;
