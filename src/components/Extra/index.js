f12f110f70e6007c03eea154788952c0d112983b
greenina <igreen0485@gmail.com>
2021-01-16 15:43:08 +0900

    feature:created image slide

---
+++ src/components/Extra/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Extra = (props)=>{
+    return(
+        <div>
+            <div>대전광역시 유성구 대학로 291 한국과학기술원</div>
+            <a href="kaist.helloworld@gmail.com">kaist.helloworld@gmail.com</a>
+        </div>
+    )
+}
+
+export default Extra;
