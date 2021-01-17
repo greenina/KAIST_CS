6b79f2ff2f1408603407cb126c847a6564fcc652
sy <nsy033@kaist.ac.kr>
2021-01-18 00:39:20 +0900

    admin logout previous page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Component } from 'react';
 import Mainpage from './components/Mainpage';
 import Memberpage from './components/MemberPage';
 import Applypage from './components/ApplyPage';
-import Adminpage from './components/AdminPage';
+import AdminPage from './components/AdminPage';
 import ApplyManagePage from './components/ApplyManagePage';
 import EventManagePage from './components/EventManagePage';
 import React, { useEffect } from 'react';
@@ -17,28 +17,51 @@ class App extends Component {
     }
   }
     
+  changeState = (flag) => {
+    console.log(this.state.mode);
+    if(flag) {
+      this.setState({ mode: 'admin' }); }
+    else {
+      this.setState({ mode: 'visitor' });
+      console.log(this.state.mode);
+      debugger; } 
+  }
+
   getContent(){
     if(this.state.mode === 'visitor') {
-      return <BrowserRouter>
-              <Switch>
-              <Route path="/" exact component={Mainpage}/>
-                <Route path="/members" exact component={Memberpage}/>
-                <Route path="/apply" exact component={Applypage}/>
-                <Route path="/administrator" exact component={Adminpage}/>
-                <Route path="/manage_events" component={EventManagePage}/>
-                <Route path="/manage_apply" component={ApplyManagePage}/>
-              </Switch>
-            </BrowserRouter>
+      return true; 
     } else if(this.state.mode === 'admin') {
-      return <Route path="/administrator" component={Adminpage} />
+      return false; 
     }
   }
 
   render() {
+    debugger;
     return (
-      <div>
-        {this.getContent()}
-      </div>
+      this.getContent() ? 
+        <BrowserRouter>
+            <Switch>
+              <Route path="/" exact render= {
+                () => <Mainpage changeState = {this.changeState} />
+              } />
+              <Route path="/members" exact component={Memberpage}/>
+              <Route path="/apply" exact component={Applypage}/>
+              <Route path="/manage_events" component={EventManagePage}/>
+              <Route path="/manage_apply" component={ApplyManagePage}/>
+              <Redirect to="/"/>
+            </Switch>
+          </BrowserRouter>
+          :
+          <BrowserRouter>
+            <Switch>
+              <Route path="/administrator" exact render= {
+                  () => <AdminPage changeState = {this.changeState} />
+                } />
+              <Route path="/manage_events" component={EventManagePage}/>
+              <Route path="/manage_apply" component={ApplyManagePage}/>
+              <Redirect to="/administrator"/>
+            </Switch> 
+          </BrowserRouter>
       );
   }
 }
