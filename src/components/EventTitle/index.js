5aaf137710164319ee2b99664f2938be8d640403
greenina <igreen0485@gmail.com>
2021-01-18 18:53:24 +0900

    get event data

--- src/components/EventTitle/index.js
+++ src/components/EventTitle/index.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './style.css'
 import 'react-slideshow-image/dist/styles.css'
-
+import axios from 'axios';
 const EventTitle = (props)=>{
+    // const { classes } = props;
+    const [eventInfo, setEventInfo] = useState({});
+    useEffect(()=>{
+        let umounted = false;
+      axios.get("http://192.249.18.245:8080/event/all").then(
+          (res)=>{
+            setEventInfo(res.data)
+          }
+      )
+      .catch(error =>{
+        console.log(error)
+      })
+      return ()=>{umounted= true}
+    },[])
+    console.log(eventInfo);
     return(
         <div id="eventtitle">
             <h1>전산학부 학생회</h1>
