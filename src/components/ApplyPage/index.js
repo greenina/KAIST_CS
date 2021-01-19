ad9ab91d2fee66ef7544ddd30906b535eb9ca22b
greenina <igreen0485@gmail.com>
2021-01-20 00:25:18 +0900

    modified Applypage style

--- src/components/ApplyPage/index.js
+++ src/components/ApplyPage/index.js
@@ -3,6 +3,8 @@ import './style.css'
 import {CKEditor} from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import axios from 'axios';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import SelectInput from '@material-ui/core/Select/SelectInput';
 class ApplyPage extends Component{
   constructor(props){
@@ -42,26 +44,48 @@ class ApplyPage extends Component{
 
     return(
       <div className="recruit">
-        <form onSubmit={this.submitHandler}>
-        <div>
-          <input onChange={this.changeHandler}  value={name} name="name" className="name-input" type="text" placeholder="이름"/>
-        </div>
-        <div>
-        <input onChange={this.changeHandler} value={studentId}name="studentId"className="studentId-input" type="text" placeholder="학번(예시:20200000)"/>
-        </div>
-        <div>
-        <input  onChange={this.changeHandler} value={phoneNum} name="phoneNum" className="phoneNum-input" type="text" placeholder="전화번호(예시:01012345678)"/>
-        </div>
-        <div>
-        <input  onChange={this.changeHandler}  value={email}name="email"className="email-input" type="text" placeholder="이메일"/>
-        </div>
-        <div>
-        <input  onChange={this.changeHandler} value={group}name="group" className="group-input" type="text" placeholder="희망 부서"/>
-        </div>
-        <div>
-        <textarea value={motive} name="motive"  onChange={this.changeHandler} className="motive-input" placeholder="지원동기를 자유롭게 작성해주세요"></textarea>
-        </div>
-        <button className="submit" type="submit" >제출</button>
+        <form  onSubmit={this.submitHandler}  alignItems="center" justify="center">
+          <Grid align="center"
+                justify="center"
+                direction="column"
+                className="format"  
+                alignItems="center" 
+                justify="center">
+            <Grid item xs={5} alignItems="center" justify="center">
+              <div className="blank"></div>
+              <Paper className="titlePaper" >
+                <div className="title">전산학부 학생회 지원서</div>
+              </Paper>
+              <div className="blank1"></div>
+              <Paper >
+                <div></div>
+                <input onChange={this.changeHandler}  value={name} name="name" className="name-input" type="text" placeholder="이름"/>
+              </Paper>
+              <div className="blank2"></div>
+              <Paper >
+                <input onChange={this.changeHandler} value={studentId}name="studentId"className="studentId-input" type="text" placeholder="학번(예시:20200000)"/>
+              </Paper>
+              <div className="blank3"></div>
+              <Paper >
+                <input  onChange={this.changeHandler} value={phoneNum} name="phoneNum" className="phoneNum-input" type="text" placeholder="전화번호(예시:01012345678)"/>
+              </Paper>
+              <div className="blank4"></div>
+              <Paper >
+                <input  onChange={this.changeHandler}  value={email}name="email"className="email-input" type="text" placeholder="이메일"/>
+              </Paper>
+              <div className="blank5"></div>
+              <Paper >
+                <input  onChange={this.changeHandler} value={group}name="group" className="group-input" type="text" placeholder="희망 부서"/>
+              </Paper>
+              <div className="blank5"></div>
+              <Paper >
+                <textarea value={motive} name="motive"  onChange={this.changeHandler} className="motive-input" placeholder="지원동기를 자유롭게 작성해주세요"></textarea>
+                <img className = 'submit' src = "/images/apply_button.png" height='50px' onClick={this.submitHandler}></img>
+              </Paper>
+            </Grid>
+            
+            {/* <button className="submit" type="submit" >제출</button> */}
+          </Grid>
         </form>
       </div>
     )
