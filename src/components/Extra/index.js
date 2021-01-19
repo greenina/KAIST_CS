dbc2ed0d0a8ebb9357e02f22cdbcdd7746617a33
sy <nsy033@kaist.ac.kr>
2021-01-20 01:23:18 +0900

    header and extra color

--- src/components/Extra/index.js
+++ src/components/Extra/index.js
@@ -7,7 +7,7 @@ class Extra extends Component{
         const Wrapper = styled.div`
             width: 100%;
             /* 색상 */
-            background: rgba(255,255,255,0.85);
+            background: rgba(251, 250, 252, 0.9);
             box-shadow: 0 3px 6px rgba(0,0,0,0.10), 0 3px 6px rgba(0,0,0,0.20);
         `;
 
